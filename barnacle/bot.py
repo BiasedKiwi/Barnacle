4d2437cd896ffc70e3199045ffb103ebee84bdc1
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-01 16:28:23 +0300

    Initial Commit

---
+++ barnacle/bot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import discord
+from discord.ext import commands
+
+
+class Barnacle:
+    def __init__(self, token="", prefix=".", case_insensitive=True, strip_after_prefix=True):
+        self.token = token
+        self.prefix = prefix
+        self.case_insensitive = case_insensitive
+        self.strip_after_prefix = strip_after_prefix
+        self.client = commands.AutoShardedBot(  # Initialise the bot instance
+            command_prefix=self.prefix,
+            case_insensitive=self.case_insensitive,
+            strip_after_prefix=self.strip_after_prefix
+        )
+
+    def set_token(self, token):
+        self.token = token
+
+    def set_prefix(self, prefix):
+        self.prefix = prefix
+
+    def set_case_insensitive(self, case_insensitive):
+        self.case_insensitive = case_insensitive
+
+    def set_strip_after_prefix(self, strip_after_prefix):
+        self.strip_after_prefix = strip_after_prefix
+
+    def start(self):
+        try:
+            self.client.run(self.token)
+        except discord.errors.LoginFailure:
+            print("An invalid token was passed.")
+            exit(1)
+        except discord.errors.GatewayNotFound:
+            print("The gateway hub for the Client websocket could not be found. This might happen when Discord is down. Please try again later.")
+            exit(1)
+            
+        # Set the events
+        
+        @self.client.event
+        def on_ready():
+            print("Barnacle is online and connected to Discord.")
+
+
+if __name__ == "__main__":
+    print("Please launch the launcher.py file instead of this one directly.")
+    exit(1)
---
+++ launcher.py
@@ -0,0 +1,39 @@
+from barnacle import Barnacle
+import os
+import sys
+from dotenv import load_dotenv
+
+
+instance = Barnacle()  # Initialise a Barnacle instance
+
+
+load_dotenv()  # Load the .env file
+if len(sys.argv) > 1:   # Set the token
+    token = sys.argv[1]
+    instance.set_token(token)
+else:  # If the token was not specified via the command line arguments, attempt to fetch it from the environment
+    token = os.environ.get("BARNACLE_TOKEN")
+    if token is None:
+        print("Please provide a valid Discord API token. (Example: 'python3 launcher.py <token> <prefix>')")
+        print("You can also set an environment variable 'BARNACLE_TOKEN' for access to the token by barnacle.")
+        exit(1)
+    instance.set_token(token)  # Set the token from an environment variable
+
+if len(sys.argv) > 2:   # Set the prefix
+    token = sys.argv[2]
+    instance.set_prefix(token)
+else:  # If the prefix was not specified via the command line arguments, attempt to fetch it from the environment
+    token = os.environ.get("BARNACLE_PREFIX")
+    if token is None:
+        print("Please provide a valid Barnacle prefix. (Example: 'python3 launcher.py <token> <prefix>')")
+        print("You can also set an environment variable 'BARNACLE_PREFIX' for access to the token by barnacle.")
+        exit(1)
+    instance.set_prefix(token)  # Set the prefix from an environment variable
+    
+strip_after_prefix = os.getenv("BARNACLE_STRIP")
+case_insensitive = os.getenv("BARNACLE_CASE")
+
+if strip_after_prefix is None or case_insensitive is None:
+    print("Warning: BARNACLE_STRIP and BARNACLE_CASE environment variables were not set. Defaulting to True for both.")
+
+instance.start()  # Start the Barnacle instance
