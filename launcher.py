a0692f53047a69210b1ca05dbdb740020e0ae051
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-01 17:36:13 +0300

    Implement logging

--- launcher.py
+++ launcher.py
@@ -2,7 +2,17 @@ from barnacle import Barnacle
 import os
 import sys
 from dotenv import load_dotenv
+import logging
+from datetime import datetime
 
+
+now = datetime.now()
+
+logger = logging.getLogger("discord")  # Set up logging
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename=f"./logs/{now.strftime('%Y-%m-%d_%H-%M-%S')}.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 instance = Barnacle()  # Initialise a Barnacle instance
 
