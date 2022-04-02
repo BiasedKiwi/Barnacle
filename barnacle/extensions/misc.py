b6424ef162624f4b72a081d49fe73d90c6c81e30
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-02 16:18:51 +0300

    Add the misc cog + ping command

---
+++ barnacle/extensions/misc.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+
+class Misc(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        print("Misc cog is online.")
+        self.bot = bot
+        
+    @commands.command(name="ping", description="Get the bot's latency in milliseconds.")
+    async def test(self, ctx):
+        """Fetches the bot's latency in milliseconds using `self.bot.latency`."""
+        embed = discord.Embed(title="Pong!", description=f"The bot's current latency is {round(self.bot.latency * 1000, 1)}ms", color=discord.Color.gold())
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Misc(bot))
