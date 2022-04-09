cd7e441fa4696bfcdb150bb6a87bda4273aba5cb
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-09 16:04:12 +0300

    Add unban command

--- barnacle/extensions/moderation/ban.py
+++ barnacle/extensions/moderation/ban.py
@@ -15,13 +15,33 @@ class Ban(commands.Cog):
     @commands.command(name="ban", description="Permanently ban a user from the server.")
     @commands.has_permissions(ban_members=True)
     @commands.bot_has_permissions(ban_members=True)
+    @commands.guild_only()
     async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = None):
         """Ban a user from a server."""
         if reason is None:
             reason = "No reason provided."
         await member.ban(reason=reason)
-        await ctx.channel.send(embed=discord.Embed(title="Done!", description=f"{member.mention} has been kicked.", color=color.gold()))
+        await ctx.channel.send(embed=discord.Embed(title="Done!", description=f"{member.mention} has been banned.", color=color.gold()))
         
+    @commands.command(name="unban", description="Unban a user from the server.")
+    @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
+    @commands.guild_only()
+    async def unban(self, ctx: commands.Context, member_id: int = None):  # TODO: Make this command support using usernames.
+        """unban a user from a server."""
+        print(member_id)
+        if member_id is not None:
+            try:
+                user: discord.User = await self.bot.fetch_user(member_id)
+                await ctx.guild.unban(user)
+            except discord.NotFound:
+                await ctx.send(embed=discord.Embed(title="Something went wrong!", description="The user ID you specified does not exist or isn't banned.", color=color.gold()))
+                return
+            await ctx.channel.send(embed=discord.Embed(title="Done!", description=f"Successfully unbanned {user.name} with ID {user.id}.", color=color.gold()))
+        else:
+            await ctx.send(embed=discord.Embed(title="Something went wrong!", description="You need to specify a user ID to unban.", color=color.gold()))
+
+    @unban.error
     @ban.error
     async def ban_handler(self, ctx: commands.Context, error: commands.CommandError):
         """The error handler for the kick command"""
@@ -35,6 +55,8 @@ class Ban(commands.Cog):
             await ctx.send(embed=discord.Embed(title="Something went wrong!", description="I do not have permission to kick users.", color=color.gold()))
         else:
             raise error
+        
+    
         
 def setup(bot: commands.Bot):
     bot.add_cog(Ban(bot))
