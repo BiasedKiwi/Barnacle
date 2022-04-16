8472aae7990705f5aa5e5abaf616293951527c2b
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-16 13:57:33 +0300

    Place pretty print code in `cog_load()` (New in dpy v2.0)

--- barnacle/extensions/misc.py
+++ barnacle/extensions/misc.py
@@ -6,17 +6,12 @@ from barnacle import PrettyPrinter
 
 class Misc(commands.Cog):
     def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    async def cog_load(self):
         self.pretty_print = PrettyPrinter()
         print("Misc cog is ", end="")
         self.pretty_print.green("online")
-        self.bot = bot
-        
-    @commands.command(name="ping", description="Get the bot's latency in milliseconds.")
-    async def test(self, ctx):
-        """Fetches the bot's latency in milliseconds using `self.bot.latency`."""
-        embed = discord.Embed(title="Pong!", description=f"The bot's current latency is {round(self.bot.latency * 1000, 1)}ms", color=discord.Color.gold())
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
         
     @commands.command(name="sync")
     @commands.is_owner()
