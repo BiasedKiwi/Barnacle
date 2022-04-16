bf0bedcd5662366d18ed2f090c12bbeb7cf6f718
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-16 13:51:33 +0300

    Make `ban` and `kick` commands slash commands

---
+++ barnacle/extensions/moderation.py
@@ -0,0 +1,104 @@
+import discord
+from discord import Color as color
+from discord import app_commands
+from discord.ext import commands
+from barnacle import PrettyPrinter
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    async def cog_load(self):
+        self.pretty_print = PrettyPrinter()
+        print("Moderation cog is ", end="")
+        self.pretty_print.green("online")
+        
+    @app_commands.command(name="kick", description="Kick a user from the server.")
+    @app_commands.checks.has_permissions(kick_members=True)
+    @app_commands.checks.bot_has_permissions(kick_members=True)
+    @app_commands.describe(member="The member to kick.", reason="Why do you want to kick this user?")
+    async def kick(self, interaction: discord.Interaction, member: discord.Member, *, reason: str = None):
+        """Kick a user from the server."""
+        if reason is None:
+            reason = "No reason provided."
+        try:
+            await member.kick(reason=reason)
+        except Exception as e:
+            embed = discord.Embed(title="Something went wrong!", description=f"Something went wrong while trying to kick {member}, make sure they are not a bot.", color=color.gold())
+            embed.add_field(name="Debug info", value=f"```{e}```")
+            await interaction.response.send_message(embed=embed)
+            return
+        await interaction.response.send_message(embed=discord.Embed(title="Done!", description=f"{member.mention} has been kicked.", color=color.gold()))
+
+    @kick.error
+    async def kick_handler(self, ctx: commands.Context, error: commands.CommandError):
+        """The error handler for the kick command"""
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(embed=discord.Embed(title="Something went wrong!", description="You do not have permission to kick users.", color=color.gold()))
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(embed=discord.Embed(title="Something went wrong!", description="You need to specify a user to kick.", color=color.gold()))
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(embed=discord.Embed(title="Something went wrong!", description="The user you specified does not exist.", color=color.gold()))
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.send(embed=discord.Embed(title="Something went wrong!", description="I do not have permission to kick users.", color=color.gold()))
+        else:
+            raise error
+        
+    @app_commands.command(name="ban", description="Permanently ban a user from the server.")
+    @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.checks.bot_has_permissions(ban_members=True)
+    @app_commands.describe(member="The member to ban.", reason="Why do you want to ban this user?")
+    async def ban(self, interaction: discord.Interaction, member: discord.Member, *, reason: str = None):
+        """Ban a user from a server."""
+        if reason is None:
+            reason = "No reason provided."
+        await member.ban(reason=reason)
+        await interaction.response.send_message(embed=discord.Embed(title="Done!", description=f"{member.mention} has been banned.", color=color.gold()))
+        
+    @app_commands.command(name="unban", description="Unban a user from the server.")
+    @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.checks.bot_has_permissions(ban_members=True)
+    @app_commands.describe(member_id="The ID of the user you would like to unban from the server.")
+    async def unban(self, interaction: discord.Interaction, member_id: int = None):  # TODO: Make this command support using usernames.
+        """unban a user from a server."""
+        if member_id is not None:
+            try:
+                user: discord.User = await self.bot.fetch_user(member_id)
+                await interaction.guild.unban(user)
+            except discord.NotFound:
+                await interaction.response.send_message(embed=discord.Embed(title="Something went wrong!", description="The user ID you specified does not exist or isn't banned.", color=color.gold()))
+                return
+            await interaction.response.send_message(embed=discord.Embed(title="Done!", description=f"Successfully unbanned {user.name} with ID {user.id}.", color=color.gold()))
+        else:
+            await interaction.response.send_message(embed=discord.Embed(title="Something went wrong!", description="You need to specify a user ID to unban.", color=color.gold()))
+
+    @unban.error
+    @ban.error
+    async def ban_handler(self, ctx: commands.Context, error: commands.CommandError):
+        """The error handler for the kick command"""
+        if isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(title="Something went wrong!", description="You do not have permission to kick users.", color=color.gold())
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title="Something went wrong!", description="You need to specify a user to kick.", color=color.gold())
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.BadArgument):
+            embed = discord.Embed(title="Something went wrong!", description="The user you specified does not exist.", color=color.gold())
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(title="Something went wrong!", description="I do not have permission to kick users.", color=color.gold())
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.errors.NoPrivateMessage):
+            embed = discord.Embed(title="Something went wrong!", description="You cannot use this command in DMs", color=color.gold())
+            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        else:
+            raise error
+        
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Moderation(bot), guilds=[discord.Object(id=883413709031108608)])
