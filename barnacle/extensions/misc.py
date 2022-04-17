d0e6dd462ce0bfcd802b1fceb41b420b2f5be369
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-17 09:03:35 +0300

    Format with black

--- barnacle/extensions/misc.py
+++ barnacle/extensions/misc.py
@@ -7,22 +7,40 @@ from barnacle import PrettyPrinter
 class Misc(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
+
     async def cog_load(self):
         self.pretty_print = PrettyPrinter()
         print("Misc cog is ", end="")
         self.pretty_print.green("online")
-        
+
     @commands.command(name="sync")
     @commands.is_owner()
     async def sync(self, ctx) -> None:
         await self.bot.tree.sync(guild=ctx.guild)
         await ctx.channel.send("Synced!")
-        
-    @app_commands.command(name="ping", description="Get the bot's latency in milliseconds.")
+
+    @sync.error
+    async def sync_handler(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.errors.NotOwner):
+            ctx.channel.send(
+                embed=discord.Embed(
+                    title="Something went wrong!",
+                    description="Only the owner of the bot can execute this command!",
+                )
+            )
+
+    @app_commands.command(
+        name="ping", description="Get the bot's latency in milliseconds."
+    )
     async def test(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Pong!", description=f"The bot's current latency is {round(self.bot.latency * 1000, 1)}ms", color=discord.Color.gold())
-        embed.set_footer(text=f"Requested by {interaction.user}", icon_url=interaction.user.avatar)
+        embed = discord.Embed(
+            title="Pong!",
+            description=f"The bot's current latency is {round(self.bot.latency * 1000, 1)}ms",
+            color=discord.Color.gold(),
+        )
+        embed.set_footer(
+            text=f"Requested by {interaction.user}", icon_url=interaction.user.avatar
+        )
         await interaction.response.send_message(embed=embed)
 
 
