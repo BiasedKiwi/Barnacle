c60f620246a3244b34cd97d86de8f31f4958b2fa
shadawcraw <shadawcraw@users.noreply.github.com>
2022-04-17 10:40:39 +0300

    Create events.py

---
+++ barnacle/extensions/events.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from barnacle import PrettyPrinter
+
+
+class EventListener(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    async def cog_load(self):
+        self.pretty_print = PrettyPrinter()
+        print("Event Listener cog is ", end="")
+        self.pretty_print.green("online")
+        
+    @commands.Cog.listener()
+    async def on_connect(self):
+        print("--------------------------------------------")
+        print("Established connection to Discord servers.")
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("\nLogged in as ", end="")
+        self.pretty_print.bold(str(self.bot.user))
+        self.pretty_print.bold("Barnacle is online and connected to Discord.")
+
+    @commands.Cog.listener()
+    async def on_shard_connect(self, shard_id: int):
+        print(f"Shard {shard_id} connected.")
+        
+    @commands.Cog.listener()
+    async def on_shard_ready(self, shard_id: int):
+        print(f"Shard {shard_id} is ready.")
+        
+    
+async def setup(bot: commands.Bot):
+    await bot.add_cog(EventListener(bot), guilds=[discord.Object(id=883413709031108608)])
